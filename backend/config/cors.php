e5ef3feb4e0dbe0a522d015189c5a38ca5a0a0ed
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-16 17:41:22 +0100

    Update CORS configuration to include localhost in allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],  // Allows all HTTP methods
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],  // Allow all origins (you can restrict this later if needed)
+    //'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],  // Allow all origins (you can restrict this later if needed)
+
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), "localhost:3000"],  // Allow all origins (you can restrict this later if needed)
 
     'allowed_origins_patterns' => [],
 
